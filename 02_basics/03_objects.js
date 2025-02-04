fc35e0a7cb7fa5f6eb7a71a22c7316346c82e256
aditya6386 <91421460+aditya6386@users.noreply.github.com>
2025-02-04 00:20:42 +0000

    objects02

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -8,11 +8,31 @@
     const jsUser = {
     name: "aditya",
     "full name": "aditya patel",
+    [mySym]: "mykey1", //(symbol should be declared by using '[]' otherwise it will be declared as string, to access it use console.log([mySym]))
     age: 18,
     location: "lucknow",
     email: "aditya@google.com",
     isloggedIn: false,
     lastLoginDays: ["monday", "tuesday"]
 }
-console.log(jsUser.email)
-console.log(jsUser["full name"]) // we cannot access the full name by using  (jsUser.fullname)  because it's declared as a string
+// console.log(jsUser.email)
+// console.log(jsUser["full name"]) // we cannot access the full name by using  (jsUser.fullname)  because it's declared as a string
+// console.log(jsUser[mySym]) // output --- mykey1
+ // -- to change or update the values --
+ jsUser.email = "adityapatel@google.com",
+//  console.log(jsUser.email);
+// Object.freeze(jsUser) // Now we cannot update the object because the object is freezed 
+jsUser.email = "adityapatel@flipkart.com" //this will not execute because the object jsUser is freezed
+// console.log(jsUser.email);
+jsUser.greeting = function(){
+    console.log("Hello jsUser");
+}
+console.log(jsUser.greeting()); //this will print hello jsUser
+console.log(jsUser.greeting); // --output-- [Function (anonymous)] // here function didn't executed but we got the reference of the function 
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello jsUser, ${this.name}`); //this is used to refer the name inside the jsUser function
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
+//.              --most of the time we use dot( . ) for accessing the value but in some cases we use square bracets []--
