8e8387bfa565ec76d67679473d51a072b5f9669a
aditya6386 <91421460+aditya6386@users.noreply.github.com>
2025-02-08 13:24:27 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,44 @@
+const user = {
+    username: "aditya",
+    price : 999,
+
+
+welcomeMessage: function(){
+    console.log(`${this.username}, welcome to website`); //this will get the value from the current context(values or variables)
+    console.log(this); // will print the current context as aditya username and after updation as sam 
+       
+}
+}  
+// user.welcomeMessage() //--output-- aditya, welcome to website
+// user.username = "sam"
+// user.welcomeMessage() //--output-- sam, welcome to website // values will be updated
+// console.log(this); // this will print the empty object {} because there is no current context in global 
+//               -- the context of this will work only in objects not in function --
+// function chai(){
+//     let username = "aditya"
+//     console.log(this.username); //output -- undefined 
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "aditya"
+//     console.log(this.username);
+// }
+
+// const chai = () => {  //arrow function
+//     let username = "aditya"
+//     console.log(this.username);  // output --undefined 
+// }
+// chai()
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+// console.log(addTwo(3, 4));
+                      //OR             
+// const addTwo = (num1, num2) => (num1 + num2) //OR  const addTwo = (num1, num2) => num1 + num2   
+// console.log(addTwo(3, 4));
+
+// when we need to return the object
+const addTwo =(num1, num2) => ({username: "aditya"})
+console.log(addTwo(3, 4));
