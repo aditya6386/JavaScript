bdb8852daf3ab7f495746351161df843ec488f0e
aditya6386 <91421460+aditya6386@users.noreply.github.com>
2025-02-11 03:59:35 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,7 +2,7 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeroes = ["shaktiman", "nagraj"]
 const myArr2 = new Array(1,2,3,4)
 // console.log(myHeroes);
-// myArr.push(6)                                 // 6 will be added at first
+// myArr.push(6)                                 // 6 will be added at last
 // myArr.pop()                                   // it will delete last element 
 // myArr.unshift(9)                              // it will add 9 at starting
 //myArr.shift()                                  // it will delete element from starting
