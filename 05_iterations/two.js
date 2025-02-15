2f0e5560f93bd8d51263dc6fe6f15e0291da8434
aditya6386 <91421460+aditya6386@users.noreply.github.com>
2025-02-15 04:39:58 +0000

    iterations_two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//                node 05_iterations/two.js
+
+// let index = 0
+// while (index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let myArr = ["Speed", "ronaldo", "messi"]
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`value is ${myArr}`);// 3 times - value is Speed,ronaldo,messi
+//     console.log(`value is ${myArr[arr]}`);//--one by one -- value is Speed, / value is ronaldo / value is messi
+//     arr++
+    
+// }
+
+
+
+
+
+
+// let score  = 11
+// do{
+//    console.log(`score is ${score}`); // it will print score is 11 one time after that the condition will not satisfy then the loop will stop
+//    score++
+// }
+// while ( score <= 10)
