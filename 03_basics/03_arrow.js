9dc387472593aa33763b5f75f8eab31070a03f8f
aditya6386 <91421460+aditya6386@users.noreply.github.com>
2025-02-16 12:43:14 +0000

    basics arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,14 +1,14 @@
-const user = {
-    username: "aditya",
-    price : 999,
+// const user = {
+//     username: "aditya",
+//     price : 999,
 
 
-welcomeMessage: function(){
-    console.log(`${this.username}, welcome to website`); //this will get the value from the current context(values or variables)
-    console.log(this); // will print the current context as aditya username and after updation as sam 
+// welcomeMessage: function(){
+//     console.log(`${this.username}, welcome to website`); //this will get the value from the current context(values or variables)
+//     console.log(this); // will print the current context as aditya username and after updation as sam 
        
-}
-}  
+// }
+// }  
 // user.welcomeMessage() //--output-- aditya, welcome to website
 // user.username = "sam"
 // user.welcomeMessage() //--output-- sam, welcome to website // values will be updated
@@ -31,14 +31,16 @@ welcomeMessage: function(){
 // }
 // chai()
 
-// const addTwo = (num1, num2) =>{
-//     return num1 + num2
+// const addTwo = (num1, num2) =>{     // if we use curly braces then we need to write return to return the values
+//     return num1 + num2            // this is a explicit return (when we use return keyword it is known as explicit return)
 // }
 // console.log(addTwo(3, 4));
                       //OR             
-// const addTwo = (num1, num2) => (num1 + num2) //OR  const addTwo = (num1, num2) => num1 + num2   
-// console.log(addTwo(3, 4));
-
-// when we need to return the object
-const addTwo =(num1, num2) => ({username: "aditya"})
+const addTwo = (num1, num2) => (num1 + num2) //OR  const addTwo = (num1, num2) => num1 + num2   
 console.log(addTwo(3, 4));
+
+// // when we need to return the object
+// const addTwo =(num1, num2) => ({username: "aditya"}) // to return the object we should wrap the object in " () "
+
+// console.log(addTwo());
+
